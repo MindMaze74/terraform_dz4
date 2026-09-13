4158b7ab15fe86b8b4d07b3ed50ad2b064880ee4
StarcevDA <isava2010@gmail.com>
2026-09-13 13:36:12 +0500

    Задание 2: remote state с S3

--- src_dz1/providers.tf
+++ src_dz1/providers.tf
@@ -1,10 +1,30 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.226.0"
     }
   }
   required_version = "~>1.12.0"
+
+  backend "s3" {
+    bucket  = "terraform-dz5-state-1789286165"
+    key     = "src_dz1/terraform.tfstate"
+    region  = "ru-central1"
+
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    access_key                  = "YC......." #бакет сикрет
+    secret_key                  = "YCP...." #бакет сикрет
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
